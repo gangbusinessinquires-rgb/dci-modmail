0ad8ce2a3cf5b90e5818f238ac83bac399cbdc4d
carter.accs <gangbusinessinquires@gmail.com>
2026-03-27 20:43:19 -0400

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
+
+const configPath = path.join(__dirname, '../../config.yml');
+
+let config;
+
+try {
+  const file = fs.readFileSync(configPath, 'utf8');
+  config = yaml.load(file);
+} catch (error) {
+  console.error(`Failed to load config: ${error.message}`);
+  process.exit(1);
+}
+
+module.exports = config;
 
