fd78262d178d690279ff94fb7be95724a3a29e77
carter.accs <gangbusinessinquires@gmail.com>
2026-03-27 20:42:17 -0400

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,1 +1,27 @@
+const { ActivityType } = require('discord.js');
+const { logInfo, logSuccess } = require('../utils/logger');
 
+module.exports = {
+  name: 'ready',
+  once: true,
+
+  async execute(client) {
+    // bot is ready
+    logSuccess(`Logged in as ${client.user.tag}`);
+    logInfo(`Serving ${client.guilds.cache.size} guild(s)`);
+    logInfo(`Watching ${client.users.cache.size} user(s)`);
+
+    // presemce
+    client.user.setPresence({
+      activities: [
+        {
+          name: 'for DMs | Modmail',
+          type: ActivityType.Watching,
+        },
+      ],
+      status: 'online',
+    });
+
+    logSuccess('DCI Modmail is online and ready.');
+  },
+};
